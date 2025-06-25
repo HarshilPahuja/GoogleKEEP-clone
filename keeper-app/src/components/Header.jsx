59bb077f3755acb46f264fb721ad8d676c753d89
HarshilPahuja <141845326+HarshilPahuja@users.noreply.github.com>
2025-06-25 20:26:37 +0530

    Update Header.jsx

--- keeper-app/src/components/Header.jsx
+++ keeper-app/src/components/Header.jsx
@@ -1,8 +1,8 @@
-
+import HighlightIcon from "@mui/icons-material/Highlight";
 export default function Header(){
     return <div>
         <header>
-            <h1>Keeper</h1>
+            <h1><HighlightIcon></HighlightIcon>Keeper</h1>
         </header>
         </div>
 }
